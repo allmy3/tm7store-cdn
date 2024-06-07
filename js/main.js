e070449e4c435fcd797c83b205ae179c23b6286c
Artem <79646356+allmy3@users.noreply.github.com>
2024-06-07 20:24:25 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById('menu');
+const headerContainer = document.getElementById('container_for_menu');
+
+function getMenu() {
+  headerContainer.classList.toggle('open');
+}
